4a3eaaf458d6a0314babcbea965ada0c237944f2
v-dumas <vincent.dumas@combodo.com>
2026-08-14 14:46:46 +0200

    🔖 Update module version to 3.4.0

--- module.approval-base.php
+++ module.approval-base.php
@@ -21,7 +21,7 @@
 /** @noinspection PhpUnhandledExceptionInspection */
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'approval-base/3.3.9',
+	'approval-base/3.4.0',
 	[
 		// Identification
 		//
